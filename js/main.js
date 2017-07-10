f73c005e22dd4e2b2ce66c70500e75df40660043
Matías <mlorenzo@xmartlabs.com>
2017-07-10 15:17:40 -0300

    Revert "shows email form upon click on google play icon"
    This reverts commit 89710f475c0ff2c9691de0fdd0cc873053ba4528.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,28 @@
-(function(){
-  $(document).ready(function(){
-    inlineSocialButtonsSvgs();
+$(document).ready(function(){
+  inlineSocialButtonsSvgs();
 
-    jQuery('#google-play-icon').click(showEmailForm)
+  // Autoplay videos
+  document.getElementsByTagName('video')[0].play();
+});
 
-    // Autoplay videos
-    document.getElementsByTagName('video')[0].play();
-  });
+function inlineSocialButtonsSvgs() {
+  $('img.social-button').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
 
-  function inlineSocialButtonsSvgs() {
-    $('img.social-button').each(function(){
-      var $img = jQuery(this);
-      var imgID = $img.attr('id');
-      var imgClass = $img.attr('class');
-      var imgURL = $img.attr('src');
-
-      jQuery.get(imgURL, function(data) {
+    jQuery.get(imgURL, function(data) {
         // Get the SVG tag, ignore the rest
         var $svg = jQuery(data).find('svg');
 
         // Add replaced image's ID to the new SVG
         if(typeof imgID !== 'undefined') {
-          $svg = $svg.attr('id', imgID);
+            $svg = $svg.attr('id', imgID);
         }
         // Add replaced image's classes to the new SVG
         if(typeof imgClass !== 'undefined') {
-          $svg = $svg.attr('class', imgClass+' replaced-svg');
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
         }
 
         // Remove any invalid XML tags as per http://validator.w3.org
@@ -33,11 +30,6 @@
 
         // Replace image with new SVG
         $img.replaceWith($svg);
-      }, 'xml');
-    });
-  }
-
-  function showEmailForm() {
-    jQuery('#email-form').css('display', 'block')
-  }
-})();
+    }, 'xml');
+  });
+}
