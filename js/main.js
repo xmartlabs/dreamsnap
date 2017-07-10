89710f475c0ff2c9691de0fdd0cc873053ba4528
Matías Lorenzo <mlvieras@hotmail.com>
2017-07-10 14:47:11 -0300

    shows email form upon click on google play icon

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
-$(document).ready(function(){
-  inlineSocialButtonsSvgs();
+(function(){
+  $(document).ready(function(){
+    inlineSocialButtonsSvgs();
 
-  // Autoplay videos
-  document.getElementsByTagName('video')[0].play();
-});
+    jQuery('#google-play-icon').click(showEmailForm)
 
-function inlineSocialButtonsSvgs() {
-  $('img.social-button').each(function(){
-    var $img = jQuery(this);
-    var imgID = $img.attr('id');
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
+    // Autoplay videos
+    document.getElementsByTagName('video')[0].play();
+  });
 
-    jQuery.get(imgURL, function(data) {
+  function inlineSocialButtonsSvgs() {
+    $('img.social-button').each(function(){
+      var $img = jQuery(this);
+      var imgID = $img.attr('id');
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
+
+      jQuery.get(imgURL, function(data) {
         // Get the SVG tag, ignore the rest
         var $svg = jQuery(data).find('svg');
 
         // Add replaced image's ID to the new SVG
         if(typeof imgID !== 'undefined') {
-            $svg = $svg.attr('id', imgID);
+          $svg = $svg.attr('id', imgID);
         }
         // Add replaced image's classes to the new SVG
         if(typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass+' replaced-svg');
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
         }
 
         // Remove any invalid XML tags as per http://validator.w3.org
@@ -30,6 +33,11 @@ function inlineSocialButtonsSvgs() {
 
         // Replace image with new SVG
         $img.replaceWith($svg);
-    }, 'xml');
-  });
-}
+      }, 'xml');
+    });
+  }
+
+  function showEmailForm() {
+    jQuery('#email-form').css('display', 'block')
+  }
+})();
